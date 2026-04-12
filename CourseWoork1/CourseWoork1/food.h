954fcf752d622deca27437b0b8e0d7cc1f25a26f
Makishima-K <70345523+Makishima-K@users.noreply.github.com>
2026-04-12 16:25:21 +0300

    Add files via upload

---
+++ CourseWoork1/CourseWoork1/food.h
@@ -0,0 +1,12 @@
+// food: id name price quantity location expiration_date 
+#include <string>
+using namespace std;
+struct food
+{
+	int id;
+	string name;
+	double price;
+	int quantity;
+	string location;
+	string expiration_date;
+};
